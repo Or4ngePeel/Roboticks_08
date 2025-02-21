9d6c9be1bcd317aede40b977f527c8fe5a9fc7d9
Or4ngePeel <kunjm971@stemcollegiate.ca>
2025-02-21 10:39:31 -0700

    Update 02_21_2025.py

--- 02_21_2025.py
+++ 02_21_2025.py
@@ -1,1 +1,8 @@
 # Today I Learned
+print ("Welcome to your adventure simulator. I am going to ask you a bunch of questions and then create an epic story with you as the star!")
+name = input ("What is your name? ")
+enemy = input ("What is your worst enemy’s name? ")
+power = input ("What is your superpower? ")
+location = input ("Where do you live? ")
+food = input ("What is your favorite food? ")
+print ("Hello", name,"!", "Your ability to", power, "will make sure you never have to look at", enemy, "again. Go eat", food, "as you walk down the streets of", location, "and use", power, "for \033[32mgood \033[0mand not \033[31mevil!")
