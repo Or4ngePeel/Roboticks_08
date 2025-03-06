2b85945e47db5315fd64b815c321886a7e8c004d
Or4ngePeel <kunjm971@stemcollegiate.ca>
2025-03-06 09:19:16 -0700

    Update 03_06_2025.py

--- 03/03/03_06_2025.py
+++ 03/03/03_06_2025.py
@@ -1,1 +1,14 @@
-# hello
+# Today I learned
+butter_chicken = input("Do you think butter chicken is spicy? ")
+if butter_chicken == "yes":
+  print("You are not indian")
+else:
+  print("You are indian")
+print("Now we will ask you one more questions")
+samosa = input("Do you like samosas? ")
+if samosa == "yes":
+  print("You are indian")
+else:
+ print("You are not indian")
+are = input("To confirm you are?")
+print("Well I have no clue who you are all I know is that you are" + are)
