a09b70dcff9780eed086835ffb1e6eb0b6b57535
Or4ngePeel <kunjm971@stemcollegiate.ca>
2025-03-11 14:10:10 -0600

    Create 2025_03_11.py

---
+++ 2025_03_11.py
@@ -0,0 +1,12 @@
+# day 6
+# today i learned
+print ("LOGIN")
+u = input ("Username: ")
+p = input ("Password: ")
+if u == "nevin" and p == "notcorrect":
+  print ("Welcome Nevin!")
+elif u == "tanish" and p == "1903":
+  print ("Welcome Tanish!")
+elif u == "kunj" and p == "hello":
+  print ("Welcome Kunj!")
+else: print ("GET OUT")
